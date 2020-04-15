32cc548f995e65a30955dc8e6db6ff2dee6b3d49
Chloe Steffens <caforonda103@yahoo.com>
2020-04-15 13:18:34 +0000

    first commit

---
+++ lib/lol_champ_stats.rb
@@ -0,0 +1,6 @@
+require "lol_champ_stats/version"
+
+module LolChampStats
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lol_champ_stats/version.rb
@@ -0,0 +1,3 @@
+module LolChampStats
+  VERSION = "0.1.0"
+end
