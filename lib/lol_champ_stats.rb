6bea30ee75e09e4beb4a4eab0f8caef86d158ef9
Chloe Steffens <caforonda103@yahoo.com>
2020-04-29 22:55:32 +0000

    modified files

--- lib/lol_champ_stats.rb
+++ lib/lol_champ_stats.rb
@@ -1,6 +1,8 @@
-require "lol_champ_stats/version"
+require_relative "lol_champ_stats/version"
 
 module LolChampStats
   class Error < StandardError; end
   # Your code goes here...
 end
+
+puts "enviornment file"
