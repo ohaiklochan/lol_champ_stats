81bd90863a701005f5d6cd4f4ff8a5dfe77898f3
Chloe Steffens <caforonda103@yahoo.com>
2020-05-12 00:28:53 +0000

    modified files

--- lib/lol_champ_stats.rb
+++ lib/lol_champ_stats.rb
@@ -1,4 +1,4 @@
-require_relative "lol_champ_stats/version"
+require_relative "./lol_champ_stats/version"
 require_relative "./lol_champ_stats/cli"
 require_relative "./lol_champ_stats/api"
 require_relative "./lol_champ_stats/champs"
