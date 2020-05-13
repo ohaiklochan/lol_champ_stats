bde17abf24a97eedab719840ddbbe70a21db4d55
Chloe Steffens <caforonda103@yahoo.com>
2020-05-13 03:34:49 +0000

    modified files

--- lib/lol_champ_stats/api.rb
+++ lib/lol_champ_stats/api.rb
@@ -1,10 +1,11 @@
 class API
   
   def self.get_data
-    response = RestClient.get("http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/champion.json")
-    champions_array = JSON.parse(response.body)["results"]
-    champions_array.each.do |champion|
-      Champion.new(champion)
+    response = Net::HTTP.get("http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/champion.json")
+    champions_array = JSON.parse(response)["champion"]
+    champions_array.each do |champion_details|
+      Champion.new(champion_name["name"], champion_title["title"], chamption_history["blurb"], champion_type["tags"], champion_stats["stats"])
+    end
   end
 
 end
